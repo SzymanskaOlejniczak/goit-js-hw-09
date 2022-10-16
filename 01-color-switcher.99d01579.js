945c96ad2d57eec8edfef9fb7848c162d42a7ef5
SzymanskaOlejniczak <105562435+SzymanskaOlejniczak@users.noreply.github.com>
2022-10-16 11:58:21 +0000

    Deploying to gh-pages from @ SzymanskaOlejniczak/goit-js-hw-09@d5b9cd44f146ef2fa783af01f630ea7372bfaf8b 🚀

---
+++ 01-color-switcher.99d01579.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.99d01579.js.map
