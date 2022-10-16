0622f8c2471834ff508dc9b4f044627b1d1989e5
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-10-16 15:40:17 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form=document.querySelector('.form');
+
+form.addEventListener('submit',onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const amountMasses=[];
+
+  const {
+    elements: { step, delay, amount  }
+    } = evt.currentTarget;
+
+  for (let i = 0; i <amount.value; i += 1){
+      const delayI=delay.value*1;
+      const stepI=step.value*1
+      const delayT=delayI + stepI*i;
+      amountMasses.push({position:i+1, delay:delayT })
   }
+
+  // console.log(amountMasses);
+
+  for (const { position, delay } of amountMasses){
+      createPromise(position,delay)
+      .then(resolve => Notiflix.Notify.success(resolve))
+      .catch(reject=>Notiflix.Notify.failure(reject));
+  }
+
+}
+
+function createPromise(position,delay){
+return new Promise ((resolve,reject)=>{
+  const shouldResolve = Math.random() > 0.3;
+
+  setTimeout(()=>{
+
+          if (shouldResolve) {
+          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          } 
+          
+          reject (`❌ Rejected promise ${position} in ${delay}ms`);
+          
+        },delay);
+
+});
+
 }
